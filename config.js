de222dda076075a61f825fdd4b85d9296dfb6bbd
gestaomedapp <gestao.medapp@gmail.com>
2026-09-01 18:38:05 -0300

    Add Firebase and Google Analytics configuration

---
+++ config.js
@@ -0,0 +1,12 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC85DuQFmxj2BQNZU4NcyJf726UrtWGIx4",
+  authDomain: "checklistmed-txxiii.firebaseapp.com",
+  projectId: "checklistmed-txxiii",
+  storageBucket: "checklistmed-txxiii.firebasestorage.app",
+  messagingSenderId: "860985456240",
+  appId: "1:860985456240:web:68785e4e26e9bb0f6be6b5",
+};
+
+export const GA4_ID = "G-9ELFCV8EGP";
+
+export const CONVITE = "Crie sua conta para salvar seu progresso em todos os aparelhos";
